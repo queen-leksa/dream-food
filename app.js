9a7a7dea796830cad633780cbccfa5fcdb7b4002
Leksa <surnacheva@ithub.ru>
2021-11-19 13:11:36 +0300

    adding pug + styl templates

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const serverRouter = require("./server/routes/mainRouter");
+const stylus = require("stylus");
+
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1>Dream Food</h1>")
-});
+app.use(stylus.middleware({
+    src: "./public",
+    dest: "./public"
+}));
+
+app.set("views", "./server/views");
+app.set("view engine", "pug");
+
+app.use(express.static("./public"));
+
+app.use("/", serverRouter);
 
 app.listen(port);
 
